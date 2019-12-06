3460e850cb02f0c734374827cf07dbb5e0630475
Daniel-Grezhdieru <grejdieru@yandex.ru>
2019-12-06 19:28:38 +0300

    grejdieru

--- api-server/models/Tag.js
+++ api-server/models/Tag.js
@@ -2,17 +2,13 @@ const mongoose = require('mongoose');
 const Types = mongoose.Schema.Types;
 const Schema = mongoose.Schema;
 
-// TODO: прописываем поля модели:
-// 0. тектс тега
-// 1. статус удаленния (признак "удалён"), булево значение, которое
-// имеет заданое заране значение.
-// ПРИМЕР:
-// x: {
-//     type: Types.Boolean,
-//     default: true
-// }
-// Документация по схемам: https://mongoosejs.com/docs/guide.html
-// Документация по моделям: https://mongoosejs.com/docs/models.html
-const schema = new Schema({}); 
+
+const schema = new Schema({
+    text: String,
+    removed: {
+        type: Types.Boolean,
+        default: true
+    }
+}); 
 
 module.exports = mongoose.model('Tag', schema);
