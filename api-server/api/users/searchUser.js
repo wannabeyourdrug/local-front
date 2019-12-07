e6bb7b64ce244657fccbaf4ea0713f86046dacb4
Daniel-Grezhdieru <grejdieru@yandex.ru>
2019-12-07 09:58:38 +0300

    Users models change tags

--- api-server/api/users/searchUser.js
+++ api-server/api/users/searchUser.js
@@ -1,3 +1,17 @@
+const buildMeta = require('../../helpers/buildMeta');
+const getToken = require('../../helpers/getToken');
+const decodeToken = require('../../helpers/decodeToken');
+const answerBuilder = require('../../helpers/answerBuilder');
+const defaultErrors = require('../../helpers/defaultErrors');
+const User = require('../../models/User');
+
+ * @param {object} req объект запроса
+ * @param {object} res объект ответа
+
+
+
+
+
 function searchUser(req, res) {}
 
 module.exports = searchUser;
--- api-server/models/User.js
+++ api-server/models/User.js
@@ -8,7 +8,7 @@ const schema = new Schema({
         unique: true
     },
     profile:{},
-    tags:[String],
+    tags:[Types.String],
     scores:{}
 });
 
