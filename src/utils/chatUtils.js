257ad12a175a6bd9d90ec58e0531aaee5a735427
stormBMO <andrey.chet76@gmail.com>
2019-12-07 17:22:18 +0300

    Chatutils pull

---
+++ src/utils/chatUtils.js
@@ -0,0 +1,17 @@
+function sendLetter(obj, socket) {
+     socket.emit('message', JSON.stringify({
+          req: {
+               "uri": "http://localhost/api/users",
+               "method": "POST",
+               "body": JSON.stringify(obj),
+               "headers": {
+                    "Content-Type": "application/json",
+                    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0YWdzIjpbXSwiX2lkIjoiNWRlYjkwM2VlMjZjNmM2OWNkZTU3ODk3IiwidXNlcm5hbWUiOiJoZWxsbyIsIl9fdiI6MCwiaWF0IjoxNTc1NzE4OTc0fQ.hLuG0ZLJb1vwg2q7lnnGQrUWC1TRg4p2lZTDb-piY2s"
+               }
+          },
+          type: 'sent'
+     }))
+     console.log("Sent")
+}
+
+export default sendLetter;
