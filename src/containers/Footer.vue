af8d7a3200eb1fcfd6b95e484bd491279b8389e2
wannabeyourdrug <wannabeyourdrug@wbydcloud.com>
2019-12-08 02:13:59 +0300

    clear footer

--- src/containers/Footer.vue
+++ src/containers/Footer.vue
@@ -4,12 +4,10 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12 col-sm-6">
-                    <p class="mb-0 text-muted">NF</p>
                 </div>
                 <div class="col-sm-6 d-none d-sm-block">
                     <ul class="breadcrumb pt-0 pr-0 float-right">
                         <li class="breadcrumb-item mb-0">
-                            <a href="#" class="btn-link">Сылка в подвале</a>
                         </li>
                         
                     </ul>
