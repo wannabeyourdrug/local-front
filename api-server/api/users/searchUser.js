58ab21ef8c710f845adb3e4bd30e8ded6e0b0641
wannabeyourdrug <wannabeyourdrug@wbydcloud.com>
2019-12-07 09:59:03 +0300

    Fone search

--- api-server/api/users/searchUser.js
+++ api-server/api/users/searchUser.js
@@ -1,3 +1,49 @@
-function searchUser(req, res) {}
+const buildMeta = require('../../helpers/buildMeta');
+const getToken = require('../../helpers/getToken');
+const decodeToken = require('../../helpers/decodeToken');
+const answerBuilder = require('../../helpers/answerBuilder');
+const defaultErrors = require('../../helpers/defaultErrors');
+const User = require('../../models/User');
+
+/**
+ * @function
+ * @name searchUser
+ * @description Функция обновляет данные о пользователе
+ * @param {object} req объект запроса 
+ * @param {object} res объект ответа
+ * @returns
+ * @exports 
+ */
+async function searchUser(req, res) {
+    // Собираем стандартный объект мета-данных
+    const meta = buildMeta(req);
+
+    const {
+        tags
+    } = req.body || {};
+
+    const token = getToken(req);
+
+    try {
+        const user = await decodeToken(token);
+
+        let users = await User.find({
+            _id: {
+                $ne: user._id
+            },
+            tags: {
+                $all: tags
+            }
+        });
+
+        if (users) {
+            answerBuilder(res, users, undefined, meta);
+        } else {
+            defaultErrors(res, 'USER_NOT_FOUND', meta);
+        }
+    } catch (error) {
+        answerBuilder(res, undefined, error, meta, 502);
+    }
+}
 
 module.exports = searchUser;
